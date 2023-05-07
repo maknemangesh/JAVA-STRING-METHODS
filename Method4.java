886249bf7870c24d5c8df47717b946d81120530e
Makne Mangesh Madhav <104612249+maknemangesh@users.noreply.github.com>
2023-05-07 17:54:30 +0530

    compareTo

---
+++ Method4.java
@@ -0,0 +1,10 @@
+// compareTo
+
+
+class CompareToDemo{
+	public static void main(String[] args){
+		String str1 = "MANGESH";
+		String str2 = "mangesh";
+		System.out.println(str1.compareTo(str2));
+	}
+}
