6e8fe9ae5b44ab707fc436268d2ae8af41c288c3
Makne Mangesh Madhav <104612249+maknemangesh@users.noreply.github.com>
2023-05-07 17:55:49 +0530

    compareToIgnore

---
+++ Method5.java
@@ -0,0 +1,10 @@
+// compareToIgnore
+
+
+class CompareToIgnore{
+	public static void main(String[] args){
+		String str1 = "MANGESH";
+		String str2 = "makne";
+		System.out.println(str1.compareToIgnoreCase(str2));
+	}
+}
