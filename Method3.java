73be2ec973aff8f7b741ed233d3d6268e9e3d4ea
Makne Mangesh Madhav <104612249+maknemangesh@users.noreply.github.com>
2023-05-07 17:53:51 +0530

    charAt

---
+++ Method3.java
@@ -0,0 +1,11 @@
+// charAt
+
+
+class CharAtDemo{
+	public static void main(String[]args){
+		String str ="MakneMangesh";
+		System.out.println(str.charAt(4));
+		System.out.println(str.charAt(7));
+	}
+}
+
