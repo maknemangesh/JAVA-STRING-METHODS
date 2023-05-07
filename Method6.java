65a346dc5b07f7f5c7afa35cda6f01396c9fb8dc
Makne Mangesh Madhav <104612249+maknemangesh@users.noreply.github.com>
2023-05-07 17:57:58 +0530

    booleanEquals

---
+++ Method6.java
@@ -0,0 +1,11 @@
+// boolean equals
+
+
+class BooleanDemo{
+	public static void main(String[] args){
+		String str1 = "Mangesh";
+		String str2 = new String("Mangesh");
+		System.out.println(str1.equals(str2));
+	}
+}
+
