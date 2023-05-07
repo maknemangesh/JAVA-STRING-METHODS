cb64a65d69361afc2d7993cc20aa3548f437228c
Makne Mangesh Madhav <104612249+maknemangesh@users.noreply.github.com>
2023-05-07 17:50:18 +0530

    concat

---
+++ Method1.java
@@ -0,0 +1,10 @@
+// Concat
+
+class ConcatDemo{
+	public static void main(String[] args){
+		String str1 = "core2";
+		String str2 = "web";
+		String str3 = str1.concat(str2);
+		System.out.println(str3);
+	}
+}
