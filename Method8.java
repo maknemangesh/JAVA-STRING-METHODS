f2b540ca5bf4586c7381c0b8ac8ddf1a501379e0
Makne Mangesh Madhav <104612249+maknemangesh@users.noreply.github.com>
2023-05-07 17:59:04 +0530

    indexOf

---
+++ Method8.java
@@ -0,0 +1,10 @@
+// indexOf
+
+
+class IndexOfDemo{
+	public static void main(String[] args){
+		String str1="mangesh";
+		System.out.println(str1.indexOf('g','0'));
+		System.out.println(str1.indexOf('s','0'));
+	}
+}
